e3d0f480d14dbf99f46c7a368094d117fd50f27c
krtu0p <109890115+krtu0p@users.noreply.github.com>
2024-08-26 08:34:08 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Select the image elements
+let stars = document.getElementById('stars');
+let moon = document.getElementById('moon');
+let mountains_behind = document.getElementById('mountains_behind');
+let mountains_front = document.getElementById('mountains_front');
+
+window.addEventListener('scroll', function() {
+    let value = window.scrollY;
+    stars.style.top = value * 0.5 + 'px';
+    moon.style.top = value * 0.8 + 'px';
+    mountains_behind.style.top = value * 0.3 + 'px';
+    mountains_front.style.top = value * 0.1 + 'px';
+});
